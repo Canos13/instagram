b460e715a20c708b76dd3c3309eff1915425781d
Canos13 <canomsergio13@gmail.com>
2022-05-15 11:07:40 -0500

    documentation

--- src/lib/View.php
+++ src/lib/View.php
@@ -4,13 +4,15 @@
     namespace Cano\Instagram\lib;
 
     class View{
-
+        /**
+         * It takes a view name and an array of data, and then requires the view file.
+         * param name The name of the view file to be rendered.
+         * param data The data that you want to pass to the view.
+         */
         function render($name, $data = [] ){
             $this->d = $data;
-            require 'src/views/'.$name.'.php';
+            require 'src/views/' .$name. '.php';
         }
-
-
     }
 
 ?>
